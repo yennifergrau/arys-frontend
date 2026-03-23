fc86fd7d12fea4cd3250d821f9d945c6882fe808
gabrieleal-beep <gabriel.leal@impulsum.me>
2026-03-23 13:03:42 -0300

    Fix en el providerHttpClient

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import { provideHttpClient } from '@angular/common/http';
 import { TextractClient } from '@aws-sdk/client-textract'
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
@@ -16,6 +17,7 @@ bootstrapApplication(AppComponent, {
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideHttpClient(),
   ],
 });
 
