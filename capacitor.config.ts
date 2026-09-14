d9bb4c583db125476e11988499fed247487a9991
Andres Leonardo Balbuena Valera <andres.balbuena@correo.unimet.edu.ve>
2026-09-14 12:16:45 -0400

    Feat: Identificador IOS

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'finanziamiento.arys',
+  appId: 'com.polizaqui.arys',
   appName: 'Arys',
   webDir: 'www'
 };
